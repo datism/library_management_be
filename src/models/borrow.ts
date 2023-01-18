2c3b7810cbcb3c7da7677a3d1d19f375728809da
d <d@d>
2023-01-18 15:53:47 +0700

    update borrow model

--- src/models/borrow.ts
+++ src/models/borrow.ts
@@ -3,27 +3,38 @@ import {Subscriber} from "./subscriber";
 import {Copy} from "./copy";
 
 interface IBorrow {
-    reader: Types.ObjectId,
-    book: Types.ObjectId,
-    at: Date,
-    isReturned: boolean
+    subscriber: Types.ObjectId,
+    copy: Types.ObjectId,
+    startDate: Date,
+    endDate: Date,
+    status: string
 }
 
 const BorrowSchema: mongoose.Schema<IBorrow> = new mongoose.Schema({
-    reader: {
+    subscriber: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: Subscriber
     },
-    book: {
+    copy: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: Copy
     },
-    at: {
+    startDate: {
         type: Date,
         required: true,
         default: Date.now
+    },
+    endDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['inProgress', 'returned', 'lost', 'overdue']
     }
 });
 
