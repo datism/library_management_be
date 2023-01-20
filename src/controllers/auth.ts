2c37d090158c1b7567a817976753e4e85c219f21
Dat Tran <trantuandat26@gmail.com>
2023-01-20 22:44:03 +0700

    fix: Raise error for mismatch password on login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,24 +1,33 @@
 import {NextFunction, Request, Response} from 'express';
-import {BadRequest} from "../error";
+import {BadRequest, Unauthorized} from "../error";
 import {User} from "../models/user";
 import jwt from "jsonwebtoken";
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-    const foundUser = await User.findOne({name: req.body.name})
+    const user = await User.findOne({name: req.body.name})
 
-    if (foundUser) {
-        const isMatch = await foundUser.comparePassword(req.body.password);
+    if (user) {
+        const isMatch = await user.comparePassword(req.body.password);
         if (isMatch) {
-            const token = jwt.sign({ _id: foundUser._id,  name: foundUser.name }, process.env.SECRET_KEY as string, {
+            const token = jwt.sign(
+                { _id: user._id,  name: user.name },
+                process.env.SECRET_KEY as string,
+                {
                 expiresIn: '2 days',
-            });
+                }
+            );
 
-            res.status(200).send({user: { _id: foundUser._id, name: foundUser.name }, token: token});
+            res.status(200).send({
+                user: { _id: user._id, name: user.name },
+                token: token,
+            });
             return next()
         }
+
+        next(new Unauthorized({message: 'Invalid password'}));
     }
 
-    next(new BadRequest({message:'Invalid credentials'}));
+    next(new Unauthorized({message:'Invalid credentials'}));
 }
 
 
