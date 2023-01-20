869eceb105e6fa0bc0942ce14a4ad970ee136808
caveYeuNghe <pekngokngekdankiu@gmail.com>
2023-01-20 10:54:43 +0700

    subscriber

--- src/controllers/subscriber.ts
+++ src/controllers/subscriber.ts
@@ -1,11 +1,26 @@
 import {NextFunction, Request, Response} from 'express';
-import {BadRequest} from "../error";
+import {BadRequest, NotFound} from "../error";
+import {Subscriber} from "../models/subscriber";
 
-export const getSubscriberById = async (req: Request, res: Response, next: NextFunction) => {
+export const getSubscribers = async (req: Request, res: Response, next: NextFunction) => {
+    const subscribers = await Subscriber.find();
 
+    if (!subscribers || subscribers.length == 0) {
+        return next(new NotFound({message:'no subscriber'}))
+    }
+
+    res.status(200).send(subscribers)
 }
 
-
 export const createSubscriber = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        await Subscriber.create({
+            ...req.body,
+        });
 
+        res.status(200).send('Inserted successfully');
+    }
+    catch (error) {
+        return next(new BadRequest({message:'Invalid credentials'}));
+    }
 }
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import {Router} from "express";
-import authRoute from "./auth";
-
-export const routes = Router();
-
-routes.use('/admin', authRoute);
